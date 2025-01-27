cbc4a1a5ef3742c9f8986ee3251d02c5486b82b7
Eugenia Domingo <eugenia.domingo@uvic.cat>
2025-01-27 18:27:12 +0100

    Fixed function and comments

--- seqClass.py
+++ seqClass.py
@@ -21,16 +21,16 @@ if re.search('^[ACGTU]+$', args.seq): #Searching the ACGTU letters in the sequen
     elif re.search('U', args.seq) and not re.search ('T', args.seq):
         print ('The sequence is RNA') #We're gonna get this message if the sequence contains a U
     else:
-        print ('The sequence can be DNA or RNA')
+        print ('The sequence can be DNA or RNA') #If the sequence does not contain a T or U, it could be either DNA or RNA. This message will appear. 
 else:
-    print ('The sequence is not DNA nor RNA') #We're gonna get this message if the sequence does contain a letter which is not AUGCT
+    print ('The sequence is not DNA nor RNA') #If the sequence does contain a letter which is not AUGCT, this message will appear
 
 
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-    print("The motif was FOUND") #When the sequence contains the motif introduced by the user
+    print("The motif was FOUND") #When the sequence contains the introduced motif, this message will appear
     else:
-        print("The motif was NOT FOUND")
+        print("The motif was NOT FOUND")#If the sequence does not contain the introduced motif, this message will appear
 
