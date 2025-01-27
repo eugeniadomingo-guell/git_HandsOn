51ed08b42df9e10288db3751e6d69704267b53a7
Eugenia Domingo <eugenia.domingo@uvic.cat>
2025-01-27 18:35:00 +0100

    Fixed function and comments on corrections

--- seqClass.py
+++ seqClass.py
@@ -16,9 +16,9 @@ args = parser.parse_args() #Using the parser function
 
 args.seq = args.seq.upper()
 if re.search('^[ACGTU]+$', args.seq): #Searching the ACGTU letters in the sequence, if there are other letters, we're gonna get the "error" message : The sequence is not DNA nor RNA 
-    if re.search('T', args.seq) and not re.search ('U', args.seq): 
+    if re.search('T', args.seq) and not re.search ('U', args.seq): #Get the sequences that contain T and not U.  
         print ('The sequence is DNA') #We're gonna get this message if the sequence contains a T
-    elif re.search('U', args.seq) and not re.search ('T', args.seq):
+elif re.search('U', args.seq) and not re.search ('T', args.seq): #Get the sequences that contain U and not T
         print ('The sequence is RNA') #We're gonna get this message if the sequence contains a U
     else:
         print ('The sequence can be DNA or RNA') #If the sequence does not contain a T or U, it could be either DNA or RNA. This message will appear. 
