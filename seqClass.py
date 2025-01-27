7b3e880927ad28bec19d6c9fe79fd5abbfbf6465
Eugenia Domingo <eugenia.domingo@uvic.cat>
2025-01-27 18:19:40 +0100

    fixed code

--- seqClass.py
+++ seqClass.py
@@ -16,9 +16,9 @@ args = parser.parse_args() #Using the parser function
 
 args.seq = args.seq.upper()
 if re.search('^[ACGTU]+$', args.seq): #Searching the ACGTU letters in the sequence, if there are other letters, we're gonna get the "error" message : The sequence is not DNA nor RNA 
-    if re.search('T', args.seq): 
+    if re.search('T', args.seq) and not re.search ('U', args.seq): 
         print ('The sequence is DNA') #We're gonna get this message if the sequence contains a T
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq) and not re.search ('T', args.seq):
         print ('The sequence is RNA') #We're gonna get this message if the sequence contains a U
     else:
         print ('The sequence can be DNA or RNA')
@@ -30,7 +30,7 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-    print("DISELOOO!") #When the sequence contains the motif introduced by the user
+    print("The motif was FOUND") #When the sequence contains the motif introduced by the user
     else:
-        print("THIS MOTIF WAS NOT FOUND")
+        print("The motif was NOT FOUND")
 
